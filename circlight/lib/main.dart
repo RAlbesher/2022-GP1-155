e7f8cccda36e92d6c4bcee2083745bc7959d9057
LamaAlmajhad <lamaalmajahd@outlook.com>
2022-10-13 22:35:44 +0300

    connection

--- circlight/lib/main.dart
+++ circlight/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
